5a615ee0ad8a2a088db0e2f71c5a9e0596f13d5c
knarhar <harutyunyanqnarik2@gmail.com>
2026-07-13 18:58:44 +0400

    Add project files.

---
+++ Shared/Protocol.cs
@@ -0,0 +1,8 @@
+﻿namespace ReaderWriterApp.Shared
+{
+    public static class Protocol
+    {
+        public const string SharedFilePath = "shared.txt";
+        public const string MessageDelimiter = "---END---";
+    }
+}
